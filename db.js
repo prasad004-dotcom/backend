ccd3b879229198ad8fb42f2a7e8323e756713a9f
Prasad <prasadlucky040904@gmail.com>
2025-11-18 13:44:52 +0530

    fix: added DB_PORT to db.js

--- db.js
+++ db.js
@@ -1,18 +1,10 @@
-const mysql = require('mysql2/promise');
-const dotenv = require('dotenv');
-dotenv.config();
-
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
+  port: process.env.DB_PORT,     // ADD THIS
   waitForConnections: true,
   connectionLimit: 10,
-  queueLimit: 0,
-  ssl: {
-    rejectUnauthorized: false
-  }
+  queueLimit: 0
 });
-
-module.exports = pool;
